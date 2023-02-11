ee9a4773918fa8702ef7c60c250a01d8d624c04e
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-02-11 01:12:17 -0800

    Replace valvelet with axios-rate-limit so that rate limit is applied across all request types.

--- services/objects.js
+++ services/objects.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import valvelet from "valvelet";
+import rateLimit from "axios-rate-limit";
 
 const QPS_LIMIT = process.env.QPS_LIMIT || 5;
 const QPS_INTERVAL = process.env.QPS_INTERVAL || 1000;
@@ -7,15 +7,20 @@ const LOCATION_HISTORY_API_BASE_URL =
   process.env.LOCATION_HISTORY_API_BASE_URL ||
   "https://api.tomtom.com/locationHistory/1";
 
+const http = rateLimit(axios.create(), {
+  maxRequests: QPS_LIMIT,
+  perMilliseconds: QPS_INTERVAL
+});
+
 const addObject = async ({ apiKey, adminKey, object }) => {
   const url = `${LOCATION_HISTORY_API_BASE_URL}/objects?key=${apiKey}&adminKey=${adminKey}`;
-  const { data } = await axios.post(url, object);
+  const { data } = await http.post(url, object);
   return data;
 };
 
 const editObject = async ({ apiKey, adminKey, id, object }) => {
   const url = `${LOCATION_HISTORY_API_BASE_URL}/objects/${id}?key=${apiKey}&adminKey=${adminKey}`;
-  const { data } = await axios.put(url, object);
+  const { data } = await http.put(url, object);
   return data;
 };
 
@@ -29,19 +34,15 @@ const getObjects = async ({ apiKey }) => {
   return objects;
 };
 
-const getObjectDetail = valvelet(
-  async ({ apiKey, id }) => {
-    const url = `${LOCATION_HISTORY_API_BASE_URL}/objects/${id}?key=${apiKey}`;
-    const { data } = await axios.get(url);
-    return data;
-  },
-  QPS_LIMIT,
-  QPS_INTERVAL
-);
+const getObjectDetail = async ({ apiKey, id }) => {
+  const url = `${LOCATION_HISTORY_API_BASE_URL}/objects/${id}?key=${apiKey}`;
+  const { data } = await http.get(url);
+  return data;
+};
 
 const deleteObject = async ({ apiKey, id }) => {
   const url = `${LOCATION_HISTORY_API_BASE_URL}/objects/${id}?key=${apiKey}`;
-  const { data } = axios.delete(url);
+  const { data } = http.delete(url);
   return data;
 };
 
