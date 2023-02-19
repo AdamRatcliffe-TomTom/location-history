3de422a43e797115db9d7d0e2ea7abed6ab36b74
Adam Ratcliffe <adam.ratcliffe@tomtom.com>
2023-02-19 12:30:27 -0800

    Allow setting QPS variables through CRA.

--- http.js
+++ http.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import rateLimit from "axios-rate-limit";
 
-const QPS_LIMIT = process.env.QPS_LIMIT || 5;
-const QPS_INTERVAL = process.env.QPS_INTERVAL || 1000;
+const QPS_LIMIT = process.env.QPS_LIMIT || process.env.REACT_APP_QPS_LIMIT || 5;
+const QPS_INTERVAL =
+  process.env.QPS_INTERVAL || process.env.REACT_APP_QPS_INTERVAL || 1000;
 
 const http = rateLimit(axios.create(), {
   maxRequests: QPS_LIMIT,
